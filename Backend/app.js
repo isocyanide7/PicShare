9dbf26209cc4daab66f531d9d7e0f314422a42d1
Isocyanide7 <nileshchandra2007@gmail.com>
2023-01-05 12:41:00 +0530

    Added code to enable CORS

--- Backend/app.js
+++ Backend/app.js
@@ -10,6 +10,16 @@ const app = express();
 
 app.use(bodyparser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
 app.use("/api/posts", postsRoutes);
 app.use("/api/users", userRoutes);
 
@@ -32,6 +42,6 @@ mongoose
   .then(() => {
     app.listen(5000);
   })
-  .catch((err) => {  
+  .catch((err) => {
     console.log(err);
   });
